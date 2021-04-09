d758ce3633cf73f84f3143379a49ead064968f61
Guillaume Haerinck <haerinck.guillaume@gmail.com>
2021-04-09 21:21:59 +0200

    [TD2] Steal intel's code

---
+++ src/tp-2/README.md
@@ -0,0 +1,9 @@
+# TD2 People counting application in realtime
+
+## Sources
+
+| Name | Type | Description |
+| --- | --- | --- |
+| [OpenCV People Counter in Python](https://www.pyimagesearch.com/2018/08/13/opencv-people-counter/) | Article | |
+| [People Counting in Real Time in Python and OpenCV](https://github.com/saimj7/People-Counting-in-Real-Time) | Github | |
+| [People counter OpenCV - Intel](https://github.com/intel-iot-devkit/people-counter-opencv) | Github | |
--- src/tp-2/main.cpp
+++ src/tp-2/main.cpp
@@ -1,44 +1,97 @@
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/video.hpp>
 #include <iostream>
-#include <stdio.h>
 
-int main(int argc, char const *argv[])
+using namespace cv;
+using namespace std;
+
+/*detects people in the area with rectangles and returns the number of people
+detected to the main function*/
+
+int detectAndDraw(const HOGDescriptor &hog, Mat &img)
 {
-    cv::Mat frame;
-    //--- INITIALIZE VIDEOCAPTURE
-    cv::VideoCapture cap;
-    // open the default camera using default API
-    // cap.open(0);
-    // OR advance usage: select any API backend
-    int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-    // open selected camera using selected API
-    cap.open(deviceID, apiID);
-    // check if we succeeded
-    if (!cap.isOpened()) {
-        std::cerr << "ERROR! Unable to open camera\n";
-        return -1;
-    }
-    //--- GRAB AND WRITE LOOP
-    std::cout << "Start grabbing" << std::endl
-        << "Press any key to terminate" << std::endl;
-    for (;;)
-    {
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
-        // check if we succeeded
-        if (frame.empty()) {
-            std::cerr << "ERROR! blank frame grabbed\n";
-            break;
-        }
-        // show live and wait for a key with timeout long enough to show images
-        imshow("Live", frame);
-        if (cv::waitKey(5) >= 0)
-            break;
-    }
+    vector<Rect> found, found_filtered;
+    double time = (double) getTickCount();
+    hog.detectMultiScale(img, found, 0, Size(8,8), Size(16,16), 1.07, 2);
+    time = (double) getTickCount() - time;
+    cout << "detection time = " << (time*1000./cv::getTickFrequency()) << " ms" << endl;
 
-    // the camera will be deinitialized automatically in VideoCapture destructor
+    for(size_t i = 0; i < found.size(); i++ )
+    {
+        Rect r = found[i];
+        size_t j;
+        // Do not add small detections inside a bigger detection.
+        for ( j = 0; j < found.size(); j++ )
+            if ( j != i && (r & found[j]) == r )
+                break;
+        if ( j == found.size() )
+        	found_filtered.push_back(r);
+    }
+    for (size_t i = 0; i < found_filtered.size(); i++)
+    {
+        Rect r = found_filtered[i];
+        // The HOG detector returns slightly larger rectangles than the real objects,
+        // so we slightly shrink the rectangles to get a nicer output.
+        r.x += cvRound(r.width*0.1);
+        r.width = cvRound(r.width*0.8);
+        r.y += cvRound(r.height*0.07);
+        r.height = cvRound(r.height*0.8);
+        rectangle(img, r.tl(), r.br(), cv::Scalar(0,255,0), 3);
+    }
+    return found_filtered.size();
+}
+
+/*captures the frames from the camera and sends to detectAndDraw to get number of
+ people entered in the area */
+
+int main(int argc, char **argv) {
+
+    HOGDescriptor hog;
+    hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+    cout << "Built with OpenCV " << CV_VERSION << endl;
+    Mat cam_image;
+    VideoCapture capture;
+    	//opening the camera
+    capture.open(0);
+         //checks whether camera is opened or not
+    if(capture.isOpened())	
+    {
+        cout << "Capture is opened" << endl;
+        for(;;)
+        {
+            capture >> cam_image;
+        //checks for the last frame to exit
+            if(cam_image.empty())	
+                break;
+	    //returns the number of people detected from detect and draw function
+            int num_people = detectAndDraw(hog, cam_image);		
+            cout << "People count: " << num_people << endl;
+        //displays every frame to the user
+            imshow("HOG Descriptor based People counter", cam_image);
+            cout<<"press q to quit"<<endl;
+            if((waitKey(30) & 0xff)== 'q')
+            {
+               cout<<"quit sucessfully"<<endl;
+               return 0;
+            }
+        }
+    }
+    else
+    {
+	//executes when the camera is not opened and exits from the main	
+        cout << "No capture" << endl;				
+        cam_image = Mat::zeros(480, 640, CV_8UC1);
+        imshow("Sample", cam_image);
+        waitKey(0);
+        return 0;
+    }
+    cout<<"video completed succesfully"<<endl;
     return 0;
 }
