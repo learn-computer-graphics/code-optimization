a324f9cae77d948f3382dec3665d50445ed2c55e
Guillaume Haerinck <haerinck.guillaume@gmail.com>
2021-04-11 17:03:36 +0200

    [TD2] Manual grayscale conversion

--- src/tp-2/main.cpp
+++ src/tp-2/main.cpp
@@ -13,11 +13,23 @@
 
 #define TRACY_NO_EXIT
 
+void convertRGBToGrayscale(const cv::Mat& in, cv::Mat& out)
+{
+	ZoneScoped
+	assert(in.cols == out.cols);
+	assert(in.rows == out.rows);
+
+	for (int i = 0; i < in.rows; i++)
+	{
+		for (int j = 0; j < in.cols; j++)
+		{
+			out.data[i * out.step + j] = 0.114 * in.data[i * in.step + j * 3] + 0.587 * in.data[i * in.step + j * 3 + 1] + 0.299 * in.data[i * in.step + j * 3 + 2];
+		}
+	}
+}
+
 int main(int argc, char** argv)
 {
-	cv::Mat frame;
-	cv::Mat segmentationMap;
-	cv::Mat temp;
 	cv::Mat kernel1(5, 5, CV_8U, 1);
 	cv::Mat kernel2(40, 40, CV_8U, 1);
 	cv::Mat labels;
@@ -32,7 +44,11 @@ int main(int argc, char** argv)
 		std::cout << "No capture" << std::endl;
 		return 0;
 	}
-	capture.set(cv::CAP_PROP_AUTO_EXPOSURE, -1);
+	const int width = capture.get(cv::CAP_PROP_FRAME_WIDTH);
+	const int height = capture.get(cv::CAP_PROP_FRAME_HEIGHT);
+	cv::Mat frame3C = cv::Mat(height, width, CV_8UC3);
+	cv::Mat frame1C = cv::Mat(height, width, CV_8UC1);
+	cv::Mat segmentationMap = cv::Mat(height, width, CV_8UC1);
 
 	bool isFirstFrame = true;
 	std::cout << "Capture is opened" << std::endl;
@@ -40,28 +56,31 @@ int main(int argc, char** argv)
 	{
 		ZoneScopedN("MainLoop")
 
-		capture.read(frame);
-		if (frame.empty())
+		capture.read(frame3C);
+		if (frame3C.empty())
 			break;
 
-		cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-
+		{
+			ZoneScopedN("ConvertToGrayscale")
+			// cv::cvtColor(frame3C, frame1C, cv::COLOR_BGR2GRAY);
+			convertRGBToGrayscale(frame3C, frame1C);
+		}
+		
 		// Init vibe
 		if (isFirstFrame)
 		{
 			isFirstFrame = false;
-			segmentationMap = cv::Mat(frame.rows, frame.cols, CV_8UC1);
 			model = libvibeModel_Sequential_New();
-			libvibeModel_Sequential_AllocInit_8u_C1R(model, frame.data, frame.cols, frame.rows);
+			libvibeModel_Sequential_AllocInit_8u_C1R(model, frame1C.data, frame1C.cols, frame1C.rows);
 		}
 		
 		// Apply Vibe background substraction
-		libvibeModel_Sequential_Segmentation_8u_C1R(model, frame.data, segmentationMap.data);
-		libvibeModel_Sequential_Update_8u_C1R(model, frame.data, segmentationMap.data);
+		libvibeModel_Sequential_Segmentation_8u_C1R(model, frame1C.data, segmentationMap.data);
+		libvibeModel_Sequential_Update_8u_C1R(model, frame1C.data, segmentationMap.data);
 		cv::medianBlur(segmentationMap, segmentationMap, 3);
 
-		cv::morphologyEx(segmentationMap, temp, cv::MORPH_OPEN, kernel1); // opening to remove noise
-		cv::morphologyEx(temp, segmentationMap, cv::MORPH_CLOSE, kernel2); // closing to fill gaps
+		cv::morphologyEx(segmentationMap, frame1C, cv::MORPH_OPEN, kernel1); // opening to remove noise
+		cv::morphologyEx(frame1C, segmentationMap, cv::MORPH_CLOSE, kernel2); // closing to fill gaps
 		cv::connectedComponentsWithStats(segmentationMap, labels, stats, centroids); // count the number of connected components
 
 		int peoples = 0;
@@ -72,17 +91,17 @@ int main(int argc, char** argv)
 			const int w = stats.at<int>(cv::Point(2, i));
 			const int h = stats.at<int>(cv::Point(3, i));
 
-			const bool isAcceptableSize = w < frame.rows - 50 && w > 50;
+			const bool isAcceptableSize = w < frame1C.rows - 50 && w > 50;
 			if (isAcceptableSize)
 			{
 				cv::Scalar color(255, 0, 0);
 				cv::Rect rect(x, y, w, h);
-				cv::rectangle(frame, rect, color);
+				cv::rectangle(frame3C, rect, color);
 				peoples++;
 			}
 		}
 
-		cv::imshow("frame", frame);
+		cv::imshow("frame", frame3C);
 		cv::imshow("Segmentation", segmentationMap);
 		std::cout << "Peoples detected : " << peoples << std::endl;
 		if (cv::waitKey(5) >= 0)
